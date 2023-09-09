a5fee701e8976d064c9e70cc4fab4e0997072bfa
Emilson Filho <emilsonfilhocontato@gmail.com>
2023-09-08 22:35:11 -0300

    Mudando título da tela de login

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ include('config/conexao.php');
   <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="assets/img/favicon.png">
   <title>
-    Login | Seleção de Alunos da Escola Profissional
+    Login | SisCO
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
