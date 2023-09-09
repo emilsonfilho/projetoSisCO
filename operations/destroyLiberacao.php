948b3f57c2552517bfcff660572e766ffc0e80a6
Emilson Filho <emilsonfilhocontato@gmail.com>
2023-09-09 12:38:53 -0300

    Feat/Adicionando a remoção de liberações
    Ainda falta alguns detalhes para evitar repetir codigo

---
+++ operations/destroyLiberacao.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+include_once('../config/conexao.php');
+
+if (!isset($_SESSION['loginuser'])) {
+    echo "Sessão inválida";
+    exit;
+}
+
+$id = $_POST['idLiberacao'];
+
+try {
+    $selectDiscenteId = "SELECT tb_jmf_discente.discente_matricula FROM tb_sisco_liberacao JOIN tb_jmf_discente ON tb_sisco_liberacao.liberacao_idDiscente = tb_jmf_discente.discente_matricula WHERE tb_sisco_liberacao.liberacao_id = :id;";
+    $stmt = $conexao->prepare($selectDiscenteId);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result) {
+        $discenteId = $result['discente_matricula'];    
+        $deleteLiberacao = "DELETE FROM tb_sisco_liberacao WHERE liberacao_id = :id";
+
+        try {
+            $stmtDelete = $conexao->prepare($deleteLiberacao);
+            $stmtDelete->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmtDelete->execute();
+
+            $rowCount = $stmtDelete->rowCount();
+
+            if ($rowCount > 0) {
+                $msgType = urlencode("success");
+                $msg = urlencode("Liberação deletada com sucesso!");
+                header("Location: ../pages/home.php?sisco=liberacao&msgType=$msgType&msg=$msg");
+            } else {
+                $msgType = urlencode("error");
+                $msg = urlencode("Falha ao deletar a liberação.");
+                header("Location: ../pages/home.php?sisco=liberacaoe&msgType=$msgType&msg=$msg");
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    } else {
+        $msgType = urlencode("error");
+        $msg = urlencode("Matrícula do discente não encontrada.");
+        header("Location: ../pages/home.php?sisco=liberacao$discenteId&msgType=$msgType&msg=$msg");
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
--- pages/detalhesLiberacao.php
+++ pages/detalhesLiberacao.php
@@ -6,7 +6,7 @@ if (isset($_GET['idLiberacao'])) {
     $idLiberacao = $_GET['idLiberacao'];
 
     // Consulta SQL para obter os detalhes da liberação com base no ID
-    $detalhesLiberacaoSelect = "SELECT tb_sisco_liberacao.liberacao_idDiscente, tb_sisco_liberacao.liberacao_idColaboradorSaida, tb_sisco_liberacao.liberacao_dtSaida, tb_sisco_liberacao.liberacao_hrSaida, tb_sisco_liberacao.liberacao_idColaboradorRetorno, tb_sisco_liberacao.liberacao_dtRetorno, tb_sisco_liberacao.liberacao_hrRetorno, tb_sisco_liberacao.liberacao_observacao, tb_jmf_discente.discente_nome AS nome_discente, tb_jmf_colaborador_saida.colaborador_nome AS nome_colaborador_saida, tb_jmf_colaborador_retorno.colaborador_nome AS nome_colaborador_retorno
+    $detalhesLiberacaoSelect = "SELECT tb_sisco_liberacao.liberacao_id AS id, tb_sisco_liberacao.liberacao_idDiscente, tb_sisco_liberacao.liberacao_idColaboradorSaida, tb_sisco_liberacao.liberacao_dtSaida, tb_sisco_liberacao.liberacao_hrSaida, tb_sisco_liberacao.liberacao_idColaboradorRetorno, tb_sisco_liberacao.liberacao_dtRetorno, tb_sisco_liberacao.liberacao_hrRetorno, tb_sisco_liberacao.liberacao_observacao, tb_jmf_discente.discente_nome AS nome_discente, tb_jmf_colaborador_saida.colaborador_nome AS nome_colaborador_saida, tb_jmf_colaborador_retorno.colaborador_nome AS nome_colaborador_retorno
                FROM tb_sisco_liberacao
                LEFT JOIN tb_jmf_discente ON tb_sisco_liberacao.liberacao_idDiscente = tb_jmf_discente.discente_matricula
                LEFT JOIN tb_jmf_colaborador AS tb_jmf_colaborador_saida ON tb_sisco_liberacao.liberacao_idColaboradorSaida = tb_jmf_colaborador_saida.colaborador_matricula
@@ -63,6 +63,17 @@ if (isset($_GET['idLiberacao'])) {
             </table>
             <a href="javascript:history.back()" class="btn btn-secondary">Voltar</a>
             <a href="home.php?sisco=editLiberacao&idLiberacao=<?php echo $idLiberacao; ?>" class="btn btn-primary">Editar Liberação</a>
+            <form action="../operations/destroyLiberacao.php" method="post" id="formDestroyLiberacao" style="display: inline;">
+                <input type="hidden" name="idLiberacao" value="<?php echo $detalhesLiberacao['id'] ?>">
+                <button type="button" class="btn btn-danger" onclick="confirmarRemocao('essa liberação', '#formDestroyLiberacao')">Remover Liberação</button>
+            </form>
         </div>
     </div>
 </div>
+<script>
+    function confirmarRemocao(tipo, idForm) {
+        if (confirm(`Tem certeza que deseja remover ${tipo}?`)) {
+            document.querySelector(idForm).submit();
+        }
+    }
+</script>
